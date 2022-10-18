fa0db3dd5db8d8d274b491ad27f9809422a08b5d
Sasha <mouss42490@gmail.com>
2022-10-18 19:20:32 +0200

    Some refactoring.

--- test_schedule/test_schedule.py
+++ test_schedule/test_schedule.py
@@ -1,14 +1,57 @@
+import json
+import os
+from pathlib import Path
+
 import schedule
+
 import time
 
-
-def task():
-    print("Job Executing!")
+from daemon import Daemon
 
 
-schedule.every(1).seconds.do(task)
+def increase_value():
+    time.sleep(2)
+    values_file_path = Path(os.path.abspath(Path(__file__).parent / "schedule.json"))
+    if not os.path.exists(values_file_path):
+        with open(values_file_path, "w", encoding="utf-8") as fopen:
+            json.dump([0], fopen)
+
+    with open(values_file_path, "r", encoding="utf-8") as fopen:
+        data = json.load(fopen)
+        data[0] = data[0] + 1
+
+    with open(values_file_path, "w", encoding="utf-8") as fopen:
+        json.dump(data, fopen)
 
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+class IncreaseValues(Daemon):
+    def __init__(self, pidfile):
+        super().__init__(pidfile)
+
+    def run(self):
+        while True:
+            increase_value()
+
+
+class ScheduledIncreaseValue(Daemon):
+    def __init__(self, pidfile):
+        super().__init__(pidfile)
+
+    def run(self):
+        schedule.every(1).seconds.do(increase_value())
+
+        while True:
+            schedule.run_pending()
+
+
+def test_increase_values():
+    b_runner = IncreaseValues(pidfile="./to_kill_ids.txt")
+    b_runner.start()
+
+
+def test_scheduled_increase_values():
+    b_runner = ScheduledIncreaseValue(pidfile="./to_kill_ids.txt")
+    b_runner.start()
+
+
+test_scheduled_increase_values()
