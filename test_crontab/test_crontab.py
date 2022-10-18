d8f9b71cf0e3c7ff030700ca1d3786b25fb035d4
Sasha <mouss42490@gmail.com>
2022-10-18 16:16:20 +0200

    Add test cron tab

---
+++ test_crontab/test_crontab.py
@@ -0,0 +1,11 @@
+import json
+import os.path
+from pathlib import Path
+cron_path = os.path.abspath(Path(__file__).parent / "cron.json")
+
+with open("./test_crontab/cron.json", "r", encoding="utf-8") as fopen:
+    data = json.load(fopen)
+    data[0] = data[0] + 1
+
+with open(cron_path, "w", encoding="utf-8") as fopen:
+    json.dump(data, fopen)
