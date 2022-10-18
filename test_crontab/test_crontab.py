ebeed2445faede3603645d7e0e0d8b432d5079c7
Sasha <mouss42490@gmail.com>
2022-10-18 22:28:03 +0200

    Some refactoring.

--- test_crontab/test_crontab.py
+++ test_crontab/test_crontab.py
@@ -1,17 +1,34 @@
+import json
 import os
+import time
 from pathlib import Path
 
 from crontab import CronTab
+import pytest
 
-parent_abs_dir = Path(os.path.abspath(Path(__file__).parent))
-log_path = parent_abs_dir / "cronlog.log"
+increase_value_file_path = os.path.abspath(Path(__file__).parent / "cron.json")
 
-if not os.path.exists(log_path):
-    with open(log_path, "w", encoding="utf-8") as fopen:
-        fopen.write("")
 
-cron = CronTab(os.environ.get("USERNAME"))
-job = cron.new(command="sh " + str(parent_abs_dir / "crontabs.sh"))
-job.minute.every(1)
-cron.write()
+@pytest.mark.skipif(os.getenv("skip_long_tests"), reason="requires python3.10 or higher")
+def test_crontab():
+    parent_abs_dir = Path(os.path.abspath(Path(__file__).parent))
+    log_path = parent_abs_dir / "cronlog.log"
 
+    if not os.path.exists(log_path):
+        with open(log_path, "w", encoding="utf-8") as fopen:
+            fopen.write("")
+
+    with open(increase_value_file_path, "r", encoding="utf-8") as fopen:
+        initial_value = json.load(fopen)[0]
+
+    cron = CronTab(os.environ.get("USERNAME"))
+    job = cron.new(command="sh " + str(parent_abs_dir / "crontabs.sh"))
+    job.minute.every(1)
+    cron.write()
+    time.sleep(65)
+
+    with open(increase_value_file_path, "r", encoding="utf-8") as fopen:
+        assert json.load(fopen)[0] > initial_value
+
+    assert os.path.getsize(log_path)
+    cron.remove_all()
--- test_crontab/test_crontab_run.py
+++ test_crontab/test_crontab_run.py
@@ -2,12 +2,11 @@ import json
 import os.path
 from pathlib import Path
 
+increase_value_file_path = os.path.abspath(Path(__file__).parent / "cron.json")
 
-cron_path = os.path.abspath(Path(__file__).parent / "cron.json")
-
-with open("./test_crontab/cron.json", "r", encoding="utf-8") as fopen:
+with open(increase_value_file_path, "r", encoding="utf-8") as fopen:
     data = json.load(fopen)
     data[0] = data[0] + 1
 
-with open(cron_path, "w", encoding="utf-8") as fopen:
+with open(increase_value_file_path, "w", encoding="utf-8") as fopen:
     json.dump(data, fopen)
