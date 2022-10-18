e27de1935c23a51479fb4dc513fbbe8ef30faa56
E044382 <sasha.keskin_ext@michelin.com>
2022-10-18 11:39:22 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# trading_bot
+
+> bash ./live_tools/install.sh
---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt install pip -y
+pip install -r live_tools/requirements.txt
+touch cronlog.log
+
+echo Install finished
